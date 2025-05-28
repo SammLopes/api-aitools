24007508698506d18035c34ec6019632f5d8465d
sam.v.p.lopes <sam.v.p.lopes@gmail.com>
2025-05-28 21:03:18 +0000

    Update prediçaõ de varias imagens e retornando para o clientes

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from ultralytics import YOLO
+
 
 app = Flask(__name__)
 
@@ -12,12 +13,12 @@ def loadModel():
 def index():
   return "Index API esta funcionando"
 
-@app.route("/predict")
+@app.route("/predict", methods=["POST"])
 def predict():
   if "image" not in request.files:
     return jsonify({'error':'Imagem não encontrada'}), 400
 
-  img_file = request.files['image']
+  img_file = request.files.getlist('images')
   img = Image.open(img_file.stream)
   
   model = loadModel()
@@ -26,17 +27,29 @@ def predict():
   boxes = results[0].boxes.data.cpu().numpy()
   class_names = model.name
 
-  predictions = []
-  for box in boxes:
-    x1, y1, x2, y2, conf, cls = box
-    predictions.append({
-      'class': class_names[int(cls)],
-      'confidence': float(conf),
-      'bbox': [int(x1), int(y1), int(x2), int(y2)]
+  result_list = []
+  for img_file in files:
+    img = Image.open(img_file.stream).convert("RGB")
+    results = model(img)
+    results[0].save(filename=f"pred_{img_file.filename}")
+
+    with open(f"pred_{img_file.filename}", "rb") as f:
+      img_bytes = f.read()
+    img_io = io.BytesIO(img_bytes)
+    result_list.append({
+      'filename':img_file.filename,
+      'img_bytes': f"/output/{img_file.filename}"
     })
   
-  return jsonify(predictions)
   
+  return jsonify(result_list )
+  
+@app.route('/output/<filename>')
+def get_output(filename):
+  path = f"pred_{filename}"
+  if not os.path.exists(path):
+    return jsonify({'error':'Imagem não encontrada'}), 400
+  return send_file(path, mimetype="image/jpeg")
 if __name__ == "__main__":
   app.run(
     debug=True, 

6c4bad4a7d88ad077a162cd8271dcf7a53f21559
SammLopes <sam.v.p.lopes@gmail.com>
2025-06-14 01:35:22 -0300

    Deixando o projeto mais enxuto

--- devserver.sh
+++ devserver.sh
@@ -2,26 +2,26 @@
 if [ "$FIREBASE_STUDIO"="studio" ]; then 
     echo "🔧 Ambiente Firebase Studio detectado"
     
-    source ./.venv/bin/activate
+    . ./.venv/bin/activate
+    pip install -r requirements.txt
 
     if [ ! -f "logs.txt" ]; then
         touch logs.txt
     fi
 
-    pip install -r requirements.txt
     python -m flask --app main run --debug
 
 else
     echo "🖥️ Ambiente local detectado"
-     # Ativa o virtualenv se existir
+     
     if [ -f ".venv/bin/activate" ]; then
         echo "👉 Ativando ambiente virtual .venv"
         . .venv/bin/activate
     else
         echo "⚠️ Ambiente virtual .venv não encontrado! Criando um novo..."
-        python3 -m venv .venv && . .venv/bin/activate
+        python -m venv .venv && . .venv/bin/activate
     fi
-    # Instala dependências
+    
     if [ -f "requirements.txt" ]; then
       echo "📦 Instalando dependências do requirements.txt"
         pip install -r requirements.txt
